25ae1546a65b53cc0e1a4ddbd444b9f275b6f9a8
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-05-04 15:15:19 -0500

    Started the tests for my_each, my_each_with_index, my_select and my_all?

---
+++ spec/enumerables_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../enumerables.rb'
+
+describe Enumerable do
+  let(:array) {[1,2,3,4,5,6,7,8,9]}
+  describe '#my_each' do
+    it 'returns each element' do
+      expect(array.my_each{ |n| n*n }).to eql(array)
+    end
+  end
+
+  describe '#my_each_with_index' do
+    it 'returns each element' do
+      expect(array.my_each_with_index{ |n| n*n }).to eql(array)
+    end
+  end
+
+  describe '#my_select' do
+    it 'returns even numbers' do
+      expect(array.select{ |n| n.even? }).to eql([2,4,6,8])
+    end
+  end
+
+  describe '#my_all' do
+    it 'returns even numbers' do
+      expect(array.my_all?{ |n| n.even? }).to eql(false)
+    end
+  end
+end
